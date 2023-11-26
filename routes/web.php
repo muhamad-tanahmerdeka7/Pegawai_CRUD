1ab9863d7a6842b0ebdc2972759c85638562933d
muhamad-tanahmerdeka7 <muhay990@gmail.com>
2023-11-26 05:15:05 -0800

    fortify dan userseeder, login logout

--- routes/web.php
+++ routes/web.php
@@ -13,23 +13,56 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
-*/
+// */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.auth.auth-login');
 });
 
-Route::get('/pegawai', [PegawaiController::class, 'index'])->name('pegawai');
 
-Route::get('/tambahdata', [PegawaiController::class, 'tambahdata'])->name('tambahdata');
+Route::middleware(['auth'])->group(function () {
+    // Pegawai
+    Route::get('home', function () {
+        return view('pages.app.dashboard-crud', ['type_menu' => '']);
+    })->name('home');
+});
 
-Route::post('/insertdata', [PegawaiController::class, 'insertdata'])->name('insertdata');
 
-Route::get('/tampildata/{id}', [PegawaiController::class, 'tampildata'])->name('itampildata');
 
-Route::post('/editdata/{id}', [PegawaiController::class, 'editdata'])->name('editdata');
 
-Route::get('/hapusdata/{id}', [PegawaiController::class, 'hapusdata'])->name('hapusdata');
-Route::get('/exportpdf', [PegawaiController::class, 'exportpdf'])->name('exportpdf');
-Route::get('/exportexcel', [PegawaiController::class, 'exportexcel'])->name('exportexcel');
-Route::post('/importexcel', [PegawaiController::class, 'importexcel'])->name('importexcel');
+
+// Route::get('/', function () {
+//     return view('pages.app.dashboard-crud', ['type_menu' => '']);
+// });
+
+// Route::get('/login', function () {
+//     return view('pages.auth.auth-login');
+// })->name('login');
+
+// Route::get('/register', function () {
+//     return view('pages.auth.auth-register');
+// })->name('register');
+
+// Route::get('/forgot', function () {
+//     return view('pages.auth.auth-forgot-password');
+// })->name('forgot');
+
+
+// Route::get('/reset-password', function () {
+//     return view('pages.auth.auth-reset-password');
+// })->name('reset-password');
+
+// Route::get('/pegawai', [PegawaiController::class, 'index'])->name('pegawai');
+
+// Route::get('/tambahdata', [PegawaiController::class, 'tambahdata'])->name('tambahdata');
+
+// Route::post('/insertdata', [PegawaiController::class, 'insertdata'])->name('insertdata');
+
+// Route::get('/tampildata/{id}', [PegawaiController::class, 'tampildata'])->name('itampildata');
+
+// Route::post('/editdata/{id}', [PegawaiController::class, 'editdata'])->name('editdata');
+
+// Route::get('/hapusdata/{id}', [PegawaiController::class, 'hapusdata'])->name('hapusdata');
+// Route::get('/exportpdf', [PegawaiController::class, 'exportpdf'])->name('exportpdf');
+// Route::get('/exportexcel', [PegawaiController::class, 'exportexcel'])->name('exportexcel');
+// Route::post('/importexcel', [PegawaiController::class, 'importexcel'])->name('importexcel');
